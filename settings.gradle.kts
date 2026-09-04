fce5c1e0227e935c0096ef1eb8f208f5e960d660
Lance Moe <admin@lance.moe>
2026-09-04 22:14:29 +0900

    fix: map multi-module directories on case-sensitive systems

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,7 @@ include(":plugins:lrclib")
 include(":modules:xposed-lrclib")
 
 project(":plugins").projectDir = file("Plugins")
+project(":modules").projectDir = file("Modules")
 project(":plugins:api").projectDir = file("Plugins/api")
 project(":plugins:lrclib").projectDir = file("Plugins/modules/lrclib")
 project(":modules:xposed-lrclib").projectDir = file("Modules/xposed-lrclib")
